d72b984baa05aaa0e637c4b125c76faefde94f36
Fernando Cladera <fcladera@fcladera.com>
2021-02-22 22:42:41 -0500

    Color for messages on run_tests.sh

--- distributed_database/scripts/test/run_tests.sh
+++ distributed_database/scripts/test/run_tests.sh
@@ -3,19 +3,26 @@
 # Echo some variables for debug
 echo "$PYTHONPATH"
 
+# Colors for better distinguishing start and end of tests
+COLOR='\033[0;35m'
+NC='\033[0m' # No Color
+
 # Run roscore in the background (and wait until it is running)
 roscore > /dev/null 2>&1 &
 echo "Waiting 2 seconds for roscore to load"
 sleep 2
 
-# Run tests in a loop
+# Run all the files that start with test_ in this folder
 for i in $(ls | grep "^test_"); do
-    echo "===================================== $i"
+    echo -e "${COLOR}===================================== $i - START${NC}"
     rosrun distributed_database "$i"
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "Error"
         exit $retVal
     fi
+    sleep 1 # To wait for some spawned processes
 done
+echo -e "${COLOR}===================================== $i - END${NC}"
+echo "ALL TESTS PASSED"
 exit 0
