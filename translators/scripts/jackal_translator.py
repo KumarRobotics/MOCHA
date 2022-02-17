e483be417a404b1406b21852d4665b22285e739b
Callisto <dcist@callisto.jackal>
2022-02-17 01:13:16 -0500

    Fixed jackal translators and added throttle for upslam

--- translators/scripts/jackal_translator.py
+++ translators/scripts/jackal_translator.py
@@ -14,5 +14,5 @@ from translator import Translator
 if __name__ == "__main__":
     rospy.init_node("jackal_translator", anonymous=True)
     path_translator = Translator("/semantic_planner/path", nav_msgs.msg.Path)
-    pose_translator = Translator("/upslam/global_pose", geometry_msgs.msg.PoseStamped)
+    pose_translator = Translator("/upslam/global_pose_throttled", geometry_msgs.msg.PoseStamped)
     rospy.spin()
