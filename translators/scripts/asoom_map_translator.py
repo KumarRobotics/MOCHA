3f0a5d0209b6dcbd6d6d3ad3b0457a465b653cf1
Callisto <dcist@callisto.jackal>
2022-02-16 23:08:36 -0500

    Fixed translators after refactoring

--- translators/scripts/asoom_map_translator.py
+++ translators/scripts/asoom_map_translator.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
-from trasnlator import Translator
+import rospy
 import nav_msgs.msg
 import sensor_msgs.msg
 import geometry_msgs.msg
+
+from translator import Translator
 
 
 if __name__ == "__main__":
--- translators/scripts/jackal_translator.py
+++ translators/scripts/jackal_translator.py
@@ -3,11 +3,13 @@
 import os
 import sys
 
+import rospy
 import sensor_msgs.msg
 import geometry_msgs.msg
 import nav_msgs.msg
 
 from translator import Translator
+
 
 if __name__ == "__main__":
     rospy.init_node("jackal_translator", anonymous=True)
