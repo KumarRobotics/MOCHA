f2fb0bbe4a1aeacd75671eff2bf89c8e5484bb03
Fernando Cladera <fcladera@fcladera.com>
2022-02-16 22:25:07 -0500

    Simplified translators for all robots

---
+++ translators/scripts/jackal_translator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+import sensor_msgs.msg
+import geometry_msgs.msg
+import nav_msgs.msg
+
+from translator import Translator
+
+if __name__ == "__main__":
+    rospy.init_node("jackal_translator", anonymous=True)
+    path_translator = Translator("/semantic_planner/path", nav_msgs.msg.Path)
+    pose_translator = Translator("/upslam/global_pose", geometry_msgs.msg.PoseStamped)
+    rospy.spin()
