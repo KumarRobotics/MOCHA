24a43a8d933fae551c0d7f283c49f03833dbd32d
Callisto <dcist@callisto.jackal>
2022-02-17 00:56:53 -0500

    Simplified topic_publisher for ddb fixed

--- topic_publisher/scripts/publish_jackal.py
+++ topic_publisher/scripts/publish_jackal.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 
-import os
-import pdb
-import sys
-
-import rospkg
-import rospy
-import yaml
-
 from topic_publisher import TopicPublisher
+import sensor_msgs.msg
+import geometry_msgs.msg
+import rospy
 
 if __name__ == "__main__":
     rospy.init_node("jackal_publisher", anonymous=True)
 
-    list_of_topics =  ["/asoom/map_sem_img": sensor_msgs.msg.Image,
-        "/asoom/map_sem_img_center": geometry_msgs.msg.PointStamped,
-        "/asoom/recent_key_pose": geometry_msgs.msg.PoseStamped]
+    list_of_topics =  {"iapetus,/asoom/map_sem_img": sensor_msgs.msg.Image,
+        "iapetus,/asoom/map_sem_img_center": geometry_msgs.msg.PointStamped,
+        "iapetus,/asoom/recent_key_pose": geometry_msgs.msg.PoseStamped}
 
     pub = TopicPublisher(list_of_topics)
     pub.run()
