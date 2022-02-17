cb1b728437899f3490927d1b66da4668182f74af
Saturn <saturn@dcist>
2022-02-17 16:57:06 -0500

    Final experiment settings

--- topic_publisher/scripts/publish_all_experiment.py
+++ topic_publisher/scripts/publish_all_experiment.py
@@ -3,6 +3,7 @@
 from topic_publisher import TopicPublisher
 import sensor_msgs.msg
 import geometry_msgs.msg
+import nav_msgs.msg
 import rospy
 
 if __name__ == "__main__":
@@ -10,7 +11,12 @@ if __name__ == "__main__":
 
     list_of_topics =  {"iapetus,/asoom/map_sem_img": sensor_msgs.msg.Image,
         "iapetus,/asoom/map_sem_img_center": geometry_msgs.msg.PointStamped,
-        "iapetus,/asoom/recent_key_pose": geometry_msgs.msg.PoseStamped}
+        "iapetus,/asoom/recent_key_pose": geometry_msgs.msg.PoseStamped,
+        "callisto,/semantic_planner/path": nav_msgs.msg.Path,
+        "callisto,/upslam/global_pose_throttled": geometry_msgs.msg.PoseStamped,
+        "europa,/semantic_planner/path": nav_msgs.msg.Path,
+        "europa,/upslam/global_pose_throttled": geometry_msgs.msg.PoseStamped
+        }
 
     pub = TopicPublisher(list_of_topics)
     pub.run()
