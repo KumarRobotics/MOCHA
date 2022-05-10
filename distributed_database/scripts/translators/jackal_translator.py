7f35366a08e39be78396a6272cc0f5f6ce4a2ab9
Saturn <saturn@dcist>
2022-05-10 16:33:42 -0400

    Mesage fixes for ICRA demo

--- distributed_database/scripts/translators/jackal_translator.py
+++ distributed_database/scripts/translators/jackal_translator.py
@@ -15,5 +15,5 @@ from translator import Translator
 if __name__ == "__main__":
     rospy.init_node("jackal_translator", anonymous=True)
     map_translator  = Translator("/object_mapper/map", sensor_msgs.msg.PointCloud2)
-    pose_translator = Translator("/object_mapper/odom", geometry_msgs.msg.PoseStamped)
+    pose_translator = Translator("/object_mapper/odom", nav_msgs.msg.Odometry)
     rospy.spin()
