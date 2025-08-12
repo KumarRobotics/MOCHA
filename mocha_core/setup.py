3ed913d7c59a64d995ae4e590f0a846f71075ed3
Fernando Cladera <fcladera@fcladera.com>
2025-08-12 13:01:59 -0400

    New file structure for mocha_core replicating ROS 2 hybrid project

---
+++ mocha_core/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+package_name = "mocha_core"
+
+setup(
+    name=package_name,
+    version="0.0.1",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="developer",
+    maintainer_email="fclad@seas.upenn.edu",
+    description="DCIST Distributed Database package",
+    license="BSD 3-Clause License",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "integrate_database = mocha_core.integrate_database:main",
+            "database_server = mocha_core.database_server:main",
+            "synchronize_channel = mocha_core.synchronize_channel:main",
+            "zmq_comm_node = mocha_core.zmq_comm_node:main",
+            "topic_publisher = mocha_core.topic_publisher:main",
+            "translator = mocha_core.translator:main",
+        ],
+    },
+)
