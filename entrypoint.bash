914c68fb0f26b4ffb136ad74d10bb91ae8bd6d3b
jason <jasonah@seas.upenn.edu>
2026-03-09 21:38:31 -0400

    added docker compose compatibility for falcon

--- entrypoint.bash
+++ entrypoint.bash
@@ -2,22 +2,24 @@
 
 HOSTNAME=$(hostname)
 
-UGVS=("phobos", "deimos", "titania", "oberon", "aphrodite", "ares")
+UGVS=("phobos" "deimos" "titania" "oberon" "aphrodite" "ares")
 UAVS=("dione")
 BASESTATIONS=("neptune" "turing")
 
 source /opt/ros/jazzy/setup.bash
-
-if [[ " ${UGVS[*]} " == *" $HOSTNAME "* ]]; then
-    ros2 launch mocha_launch jackal.launch.py robot_name:=$HOSTNAME
-
-elif [[ " ${UAVS[*]} " == *" $HOSTNAME "* ]]; then
-    ros2 launch mocha_launch titan.launch.py robot_name:=$HOSTNAME
-
-elif [[ " ${BASESTATIONS[*]} " == *" $HOSTNAME "* ]]; then
-    ros2 launch mocha_launch basestation.launch.py robot_name:=basestation
-
-else
-    echo "Error: Hostname '$HOSTNAME' not recognized."
-    exit 1
+source ws/install/setup.bash
+if [ "${MOCHA}" == "true" ]; then
+    if [[ " ${UGVS[*]} " == *" $HOSTNAME "* ]]; then
+        ros2 launch mocha_launch jackal.launch.py robot_name:=$HOSTNAME
+    
+    elif [[ " ${UAVS[*]} " == *" $HOSTNAME "* ]]; then
+        ros2 launch mocha_launch titan.launch.py robot_name:=$HOSTNAME
+    
+    elif [[ " ${BASESTATIONS[*]} " == *" $HOSTNAME "* ]]; then
+        ros2 launch mocha_launch basestation.launch.py robot_name:=basestation
+    
+    else
+        echo "Error: Hostname '$HOSTNAME' not recognized."
+        exit 1
+    fi
 fi
