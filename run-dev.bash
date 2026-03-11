43309b5740e249ff4acdca0437d3e12fefa48454
jason <jasonah@seas.upenn.edu>
2026-03-10 22:03:11 -0400

    fix misspelling

--- run-dev.bash
+++ run-dev.bash
@@ -6,6 +6,7 @@ docker run --rm -it \
   --network=host \
   --privileged \
   --entrypoint="" \
+  -e MOCHA=true \
   --name dtc-platform-`hostname`-mocha \
   dtc-platform-`hostname`:mocha \
   bash
